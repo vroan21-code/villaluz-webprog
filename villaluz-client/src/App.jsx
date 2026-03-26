6717848b177aeced81752fbf18947781c62a4bba
Roan Villaluz <vroan21@gmail.com>
2026-03-26 21:11:14 +0800

    Lab Act 1

---
+++ villaluz-client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from "./assets/vite.svg";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Roan James Villaluz<br />
+          Email: vroan21@gmail.com<br />
+          Other Personal Info: HATDOG <br />
+          <a href='https://github.com/vroan21-code/Villaluz_Client'>My GitHub Profile</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+export default App
